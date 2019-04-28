0a1a56bd83b9061010307f8fd0367751dc4dbd1c
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-27 22:07:44 -0300

    Criação e editando as animações do personagem.
    Criação das animações: parado, atacando, pulando, caindo e caminhando.
    Alteração nos scripts para iniciar as animações.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRb;
     public float speed, jumpForce;
-    public bool isLookLeft;
+    public bool isLookLeft, isGrounded;
+    public Animator playerAnimator;
+    public Transform groundCheck;
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -24,12 +27,25 @@ public class PlayerController : MonoBehaviour
 
         float speedY = playerRb.velocity.y;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") &&  isGrounded)
         {
             playerRb.AddForce(new Vector2(0, jumpForce));
         }
 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            playerAnimator.SetTrigger("attack");
+        }
+
         playerRb.velocity = new Vector2(h * speed, speedY);
+
+        playerAnimator.SetInteger("horizontal", (int)h);
+        playerAnimator.SetBool("isGrounded", isGrounded);
+        playerAnimator.SetFloat("speedY", speedY);
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
     }
 
     private void Flip()
