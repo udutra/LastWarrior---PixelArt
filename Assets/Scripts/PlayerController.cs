372a13d687ed4bf709a77a372caac211e9985c44
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-27 21:24:13 -0300

    Mmecânicas básica do personagem.
    Criação do Script do Player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public float speed, jumpForce;
+    public bool isLookLeft;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if ((h > 0 && isLookLeft) || (h < 0 && !isLookLeft))
+        {
+            Flip();
+        }
+
+        float speedY = playerRb.velocity.y;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRb.AddForce(new Vector2(0, jumpForce));
+        }
+
+        playerRb.velocity = new Vector2(h * speed, speedY);
+    }
+
+    private void Flip()
+    {
+        isLookLeft = !isLookLeft;
+        float x = transform.localScale.x * -1;
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+}
